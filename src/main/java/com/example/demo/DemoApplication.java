771d8eeaca4e6238429e4661b95226f68cd73f3f
saddem yassin <saddemyassin@gmail.com>
2018-10-30 01:13:49 +0100

    add Providers

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.example.demo.repristory")
-@EntityScan(basePackages = "com.example.demo.entity")
 public class DemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/demo/entity/Provider.java
@@ -0,0 +1,68 @@
+package com.example.demo.entity;
+
+import org.hibernate.validator.constraints.UniqueElements;
+
+import javax.persistence.*;
+
+@Entity
+public class Provider {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+    private String ville;
+    private String email;
+    private Integer numTel;
+
+
+
+    public Provider(String name, String ville, String email, Integer numTel) {
+        this.name = name;
+        this.ville = ville;
+        this.email = email;
+        this.numTel = numTel;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getNumTel() {
+        return numTel;
+    }
+
+    public void setNumTel(Integer numTel) {
+        this.numTel = numTel;
+    }
+}
